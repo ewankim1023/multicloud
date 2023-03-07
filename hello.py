63844f031db4d2e61a9cff62f0b605751218d849
Taeyoon Kim <ewankim1023@gmail.com>
2023-03-07 16:43:53 +0000

    add marco polo test

--- hello.py
+++ hello.py
@@ -1,4 +1,19 @@
-def speak(name):
-    return "My name is %s" % name
+import click
 
-print(speak("Taeyoon"))
+def logic(name):
+    if name == "Marco":
+        return "Polo"
+    else:
+        return "No Match"
+
+@click.command()
+@click.option("--name")
+def marco(name):
+    """This is a Marco Polo function"""
+    result = logic(name)
+    click.echo(result)
+
+
+if __name__ == "__main__":
+    # pylint: disable=no-value-for-parameter
+    marco()
